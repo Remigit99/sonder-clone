fb20377774244b3f26acde6af86719c8381480ed
Remigit99 <aderemibiodun1000@gmail.com>
2023-04-14 12:52:56 +0100

    started the search and check section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -22,50 +22,56 @@ import { Autoplay, Pagination, EffectFade } from "swiper";
 
 const Home = () => {
     return (
-        <Swiper
+        <>
+            <Swiper
 
-            spaceBetween={0}
-            effect={'fade'}
-            centeredSlides={true}
-            autoplay={{
-                delay: 3500,
-                disableOnInteraction: false,
-            }}
-            pagination={{
-                clickable: true,
-            }}
+                spaceBetween={0}
+                effect={'fade'}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 3500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
 
-            modules={[Autoplay, Pagination, EffectFade]}
-            className="mySwiper"
+                modules={[Autoplay, Pagination, EffectFade]}
+                className="mySwiper "
 
-        >
-            {
-                HeaderSlideData.map(({ id, location, stayStatus, slideImg }) => {
-                    return (
+            >
+                {
+                    HeaderSlideData.map(({ id, location, stayStatus, slideImg }) => {
+                        return (
 
-                        <SwiperSlide className="hero__slide" key={id}>
-                            <div className="slide__img">
-                                <img src={slideImg} alt={location} className='slider__img' />
-                            </div>
+                            <SwiperSlide className="hero__slide" key={id}>
+                                <div className="slide__img">
+                                    <img src={slideImg} alt={location} className='slider__img' />
+                                </div>
 
-                            <div className="hero__overlay-text">
-                                <h1>A better way to stay</h1>
-                                <p>
-                                    Inspiring, award-winning design meets modern, mobile-first service. Welcome to the future of hospitality.
-                                </p>
+                                <div className="hero__overlay-text">
+                                    <h1>A better way to stay</h1>
+                                    <p>
+                                        Inspiring, award-winning design meets modern, mobile-first service. Welcome to the future of hospitality.
+                                    </p>
 
-                                <small>
-                                    <TfiLocationPin className="location__icon" />  {location} | {stayStatus}
-                                </small>
-                            </div>
-                        </SwiperSlide>
+                                    <small>
+                                        <TfiLocationPin className="location__icon" />  {location} | {stayStatus}
+                                    </small>
+                                </div>
+                            </SwiperSlide>
 
-                    )
-                })
+                        )
+                    })
 
 
-            }
-        </Swiper>
+                }
+            </Swiper>
+
+            <div className="search__space">
+
+            </div>
+        </>
     )
 }
 
