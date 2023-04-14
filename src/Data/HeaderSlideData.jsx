bade57a0d7aa4bb002388e3abfb7267e78c7c568
Remigit99 <aderemibiodun1000@gmail.com>
2023-04-14 11:54:12 +0100

    created data folder

---
+++ src/Data/HeaderSlideData.jsx
@@ -0,0 +1,39 @@
+
+import heroSlide1 from '../assets/hero-slide-01.jpg'
+import heroSlide2 from '../assets/hero-slide-02.jpg'
+import heroSlide3 from '../assets/hero-slide-03.jpg'
+import heroSlide4 from '../assets/hero-slide-04.jpg'
+import heroSlide5 from '../assets/hero-slide-05.jpg'
+
+export const HeaderSlideData = [
+    {
+        id: 1,
+        location: 'Phoenix',
+        stayStatus: '1 bedroom stay',
+        slideImg: heroSlide1
+    },
+    {
+        id: 2,
+        location: 'Rome',
+        stayStatus: '2 bedroom stay',
+        slideImg: heroSlide2
+    },
+    {
+        id: 3,
+        location: 'Philadelphia',
+        stayStatus: '2 bedroom stay',
+        slideImg: heroSlide3
+    },
+    {
+        id: 4,
+        location: 'London',
+        stayStatus: '3 bedroom stay',
+        slideImg: heroSlide4
+    },
+    {
+        id: 5,
+        location: 'Mexico City',
+        stayStatus: 'Queen room stay',
+        slideImg: heroSlide5
+    },
+]
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,72 @@
 
 import React from 'react';
 import '../Home/Home.css';
+import { HeaderSlideData } from '../../Data/HeaderSlideData';
+// import LocationIcon from '../../assets/vector7.svg';
+import { TfiLocationPin } from 'react-icons/tfi';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
+// import required modules
+import { Autoplay, Pagination, EffectFade } from "swiper";
+
+
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <Swiper
+
+            spaceBetween={0}
+            effect={'fade'}
+            centeredSlides={true}
+            autoplay={{
+                delay: 3500,
+                disableOnInteraction: false,
+            }}
+            pagination={{
+                clickable: true,
+            }}
+
+            modules={[Autoplay, Pagination, EffectFade]}
+            className="mySwiper"
+
+        >
+            {
+                HeaderSlideData.map(({ id, location, stayStatus, slideImg }) => {
+                    return (
+
+                        <SwiperSlide className="hero__slide" key={id}>
+                            <div className="slide__img">
+                                <img src={slideImg} alt={location} className='slider__img' />
+                            </div>
+
+                            <div className="hero__overlay-text">
+                                <h1>A better way to stay</h1>
+                                <p>
+                                    Inspiring, award-winning design meets modern, mobile-first service. Welcome to the future of hospitality.
+                                </p>
+
+                                <small>
+                                    <TfiLocationPin className="location__icon" />  {location} | {stayStatus}
+                                </small>
+                            </div>
+                        </SwiperSlide>
+
+                    )
+                })
+
+
+            }
+        </Swiper>
     )
 }
 
-export default Home
+export default Home;
